b2bd14427996f1ac2fb9104c9dbc35b3fba8541f
Marc Cooperstein <mcooperstein@gmail.com>
2018-03-17 17:48:35 -0700

    add support for CHANGE_SEARCH_TEXT action

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -5,9 +5,26 @@ console.log('Starting todo redux example');
 var stateDefault = {todos:[],showCompleted:false,searchText:''}
 var reducer = (state= stateDefault,action) =>{
   // state = state || {name: 'Anonymous'};
-  return state;
+  switch(action.type){
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+    default:
+      return state;
+  }
 }
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+var action = {
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Dog'
+};
+
+store.dispatch(action);
+
+console.log('Search Text should be Dog', store.getState());
